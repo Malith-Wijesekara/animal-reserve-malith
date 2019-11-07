3f9a481f1d31dc0d34e27441e97786edc6c505c9
Malith-Wijesekara <154170x@uom.lk>
2019-11-07 13:00:22 +0530

    fiixed

--- animal-reserve-malith/lambda.js
+++ animal-reserve-malith/lambda.js
@@ -1,20 +1,22 @@
-let AWS = require('aws-sdk');
+let AWS = require("aws-sdk");
 let rekog = new AWS.Rekognition();
 
 exports.handler = function(event, context, callback) {
-    //console.log(JSON.stringify(event,null,2));
+    //console.log(JSON.stringify(event, null, 2));
+
     let s3 = event.Records[0].s3;
     rekog.detectLabels({
-        Image:{
-            S3Object:{
-                Bucket:s3.Bucket.name,
-                Name: s3.Object.key
+        Image: {
+            S3Object: {
+                Bucket: s3.bucket.name,
+                Name: s3.object.key
             }
         },
-        MaxLabels:1
-    }).promise().then(data =>{
+        MaxLabels: 1
+    }).promise()
+    .then(data => {
         console.log(data);
         callback(null, {});
-    }).catch(err => callback(err));
-    callback(null, event);//how we signal back to the system
+    })
+    .catch(err => callback(err));
 }
