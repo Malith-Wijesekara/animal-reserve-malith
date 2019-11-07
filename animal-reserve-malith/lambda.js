3b29d501d428e824a73dddffa51267a41a22632c
Malith-Wijesekara <154170x@uom.lk>
2019-11-07 13:13:48 +0530

    DynamoDB added

--- animal-reserve-malith/lambda.js
+++ animal-reserve-malith/lambda.js
@@ -1,7 +1,8 @@
 let AWS = require("aws-sdk");
+const ddb = new AWS.DynamoDB.DocumentClient();
 let rekog = new AWS.Rekognition();
 
-exports.handler = function(event, context, callback) {
+exports.handler = function (event, context, callback) {
     //console.log(JSON.stringify(event, null, 2));
 
     let s3 = event.Records[0].s3;
@@ -14,9 +15,25 @@ exports.handler = function(event, context, callback) {
         },
         MaxLabels: 1
     }).promise()
-    .then(data => {
-        console.log(data);
-        callback(null, {});
-    })
-    .catch(err => callback(err));
+        .then(data => {
+            console.log(data);
+            let label = data.Labels[0].Name;
+            ddb.put({
+                TableName: 'slp-animals-malith',
+                Item: {
+                    'name': s3.object.key,
+                    'label': label
+                }
+            }).promise()
+                .then((data) => {
+                    //your logic goes here
+                    callback(null, {});
+                })
+                .catch((err) => {
+                    //handle error
+                    callback(err);
+                });
+            callback(null, {});
+        })
+        .catch(err => callback(err));
 }
